f53cf940059d1bf5f45ceab2c9b94a06762c00a6
IlTelegatto <69989584+IlTelegatto@users.noreply.github.com>
2024-05-28 23:21:35 +0200

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,13 @@
+body {
+    margin: 0;
+    padding: 0;
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-family: Arial, sans-serif;
+    transition: background-image 1s ease-in-out;
+}
