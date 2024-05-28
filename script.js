e676a899500a9ba0c3828eccd7e5f27d94bb234f
IlTelegatto <69989584+IlTelegatto@users.noreply.github.com>
2024-05-29 00:09:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const images = [
     'ciao.png',
     'image/1.png',
     'image/2.png',
-    'immagine4.jpg'
+    'image/3.png',
+    'image/4.png',
+    'image/5.png',
+    'image/6.png',
+    'image/7.png',
+    'image/8.png',
+    'image/9.png',
+    'image/10.png',
+    'image/11.png',
+    'image/12.png',
+    'image/13.png',
+    'image/14.png',
 ];
 
 let currentIndex = 0;
